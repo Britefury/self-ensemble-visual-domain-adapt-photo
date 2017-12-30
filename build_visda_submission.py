59dc274158e831a83794a847a756c6af2c5b64ba
Geoffrey French <brittix1023@gmail.com>
2017-12-30 22:28:40 +0000

    VisDA submission builder generates the output ZIP file in one step.

--- build_visda_submission.py
+++ build_visda_submission.py
@@ -3,12 +3,15 @@ import click
 
 @click.command()
 @click.argument('output', type=click.File('wb'))
+@click.argument('source_pred_path', type=click.Path(exists=True))
 @click.argument('predictions_paths', type=click.Path(exists=True), nargs=-1)
 @click.option('--no_aug', is_flag=True, default=False)
 @click.option('--epoch', type=int, default=None)
-def build_submission(output, predictions_paths, no_aug, epoch):
+def build_submission(output, source_pred_path, predictions_paths, no_aug, epoch):
+    import os
     import numpy as np
     import tables
+    import zipfile
 
 
     def get_best_y(path):
@@ -25,23 +28,28 @@ def build_submission(output, predictions_paths, no_aug, epoch):
             return f_target_pred.root.target_pred_y.y_prob_aug
 
 
-    best_ys = [get_best_y(path) for path in predictions_paths]
+    def write_predictions(dest_file, src_paths):
+        if len(src_paths) == 1 and os.path.splitext(src_paths[0])[1] == '.txt':
+            data = open(src_paths[0], 'rb').read()
+            dest_file.write(data)
+        else:
+            best_ys = [get_best_y(path) for path in src_paths]
+
+            # Average
+            best_y = np.mean(np.array(best_ys), axis=0)
+
+            pred_y_cls = np.argmax(best_y, axis=1)
+
+            print('Obtained predictions')
 
 
-    print('Loaded predictions')
+            for i in range(len(pred_y_cls)):
+                dest_file.write('{}\n'.format(pred_y_cls[i]).encode('us-ascii'))
 
-    # Average
-    best_y = np.mean(np.array(best_ys), axis=0)
-
-    pred_y_cls = np.argmax(best_y, axis=1)
-
-    print('Obtained predictions')
-
-
-    for i in range(len(pred_y_cls)):
-        output.write('{}\n'.format(pred_y_cls[i]).encode('us-ascii'))
-
-
+    z = zipfile.ZipFile(output, mode='w')
+    write_predictions(z.open('source_results.txt', 'w'), [source_pred_path])
+    write_predictions(z.open('adaptation_results.txt', 'w'), predictions_paths)
+    z.close()
 
 
 if __name__ == '__main__':
